3eec386316da0d40a60970267068827993d07130
DeepakChauhan33 <deep651965@gmail.com>
2025-09-03 23:45:29 +0530

    Update3

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -11,10 +11,9 @@ const Welcome = () => {
 
 
 
-            <main className="flex justify-center items-center h-dvh w-screen bg-gray-100">
+            <main className='h-dvh sm:flex sm:justify-center sm:items-center sm:h-screen sm:w-screen'>
 
-
-        <div className="p-4 flex flex-col justify-end gap-3 w-full h-full sm:h-[75%] sm:min-w-[380px] sm:rounded-lg sm:shadow-lg bg-white">
+            <div className='p-4 flex flex-col justify-end  h-full gap-3 sm:h-[75%] sm:min-w-[380px] sm:rounded-lg sm:shadow-lg bg-gray-100'>
                     <h1 className="text-2xl font-bold mb-2">Welcome to PopX</h1>
                     <p className="text-gray-600 mb-6 ">
                         Lorem ipsum dolor sit amet,<br />
